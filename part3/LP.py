a8cc0cf8c224d61c856a39f33599c259261c5f81
Vasu Singhal <vasusinghal0403@gmail.com>
2020-04-12 01:42:54 +0530

    added code for A matrix

--- part3/LP.py
+++ part3/LP.py
@@ -1,11 +1,12 @@
 class LinearProgram:
+
 	def initialize_states(self):
 		for enemy_health in range(5):
 			for number_of_arrows in range(4):
 				for stamina in range(3):
-					self.states.append([enemy_health, number_of_arrows, stamina])				
+					self.states.append((enemy_health, number_of_arrows, stamina))		
 	
-	def initialize_possible_states(self):
+	def initialize_possible_actions(self):
 		for cur_state in self.states:
 			enemy_health = cur_state[0]
 			number_of_arrows = cur_state[1]
@@ -21,7 +22,59 @@ class LinearProgram:
 				if stamina < 2:
 					cur_possible_actions.append(self.actions["RECHARGE"])
 			cur_possible_actions.sort()
-			self.possible_actions.append(cur_possible_actions)
+			self.possible_actions[cur_state] = cur_possible_actions
+
+	def flow(self, initial_state, final_state, action):
+		initial_enemy_health = initial_state[0]
+		initial_number_of_arrows = initial_state[1]
+		initial_stamina = initial_state[2]
+
+		final_enemy_health = final_state[0]
+		final_number_of_arrows = final_state[1]
+		final_stamina = final_state[2]
+	
+		action = self.convert_back[action]
+		if initial_state == final_state:		
+			if action == "NOOP":
+				return 1
+			elif action == "RECHARGE":
+				return 0.8
+			elif action == "DODGE":
+				return 1
+			elif action == "SHOOT":
+				return 1
+		else:
+			if action == "NOOP":
+				return 0
+			elif action == "RECHARGE":
+				if final_number_of_arrows == initial_number_of_arrows and final_stamina == initial_stamina + 1 and final_enemy_health == initial_enemy_health:
+					return -0.8
+				else:
+					return 0
+			elif action == "DODGE":
+				if final_number_of_arrows == initial_number_of_arrows and (final_stamina == initial_stamina - 1 or final_stamina == initial_stamina - 2) and final_enemy_health == initial_enemy_health:
+					return -0.1
+				elif final_number_of_arrows == initial_number_of_arrows + 1 and (final_stamina == initial_stamina - 1 or final_stamina == initial_stamina - 2) and final_enemy_health == initial_enemy_health:
+					return -0.4
+				else:
+					return 0
+			elif action == "SHOOT":
+				if final_number_of_arrows == initial_number_of_arrows - 1 and final_stamina == initial_stamina - 1 and final_enemy_health == initial_enemy_health:
+					return -0.5
+				elif final_number_of_arrows == initial_number_of_arrows - 1 and final_stamina == initial_stamina - 1 and final_enemy_health == initial_enemy_health - 1:
+					return -0.5
+				else:
+					return 0
+
+	def initialize_Amatrix(self):
+		for final_state in self.states:
+			cur_list = []
+			for initial_state in self.states:
+				for action in self.possible_actions[initial_state]:
+					cur_list.append(self.flow(initial_state, final_state, action))
+			self.A.append(cur_list);
+
+
 
 	def __init__(self):
 		self.order_of_states = ["enemy_health", "number_of_arrows", "stamina"]
@@ -31,11 +84,18 @@ class LinearProgram:
 			"DODGE": 2,
 			"SHOOT": 3
 		}
+		self.convert_back = {
+			0: "NOOP",
+			1: "RECHARGE",
+			2: "DODGE",
+			3: "SHOOT"
+		}
 		self.states = []
 		self.A = []
-		self.possible_actions = []
+		self.possible_actions = {}
 
 linearProgram = LinearProgram()
 linearProgram.initialize_states()
-linearProgram.initialize_possible_states()
-print(linearProgram.possible_actions)
+linearProgram.initialize_possible_actions()
+linearProgram.initialize_Amatrix()
+print(linearProgram.A)
