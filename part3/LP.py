fixed error in A matrix calculation

part3/LP.py
@@ -30,7 +30,7 @@ class LinearProgram:
         self.A = []
         self.reward = []
         self.possible_actions = {}
-        self.penalty = -10
+        self.penalty = -20
         self.alpha = []
         self.total_actions = 0
         self.x = []
@@ -115,6 +115,7 @@ class LinearProgram:
                         return -0.2
 
                     elif final_number_of_arrows == initial_number_of_arrows and final_enemy_health == initial_enemy_health:
+
                         if final_stamina == initial_stamina - 1:
                             return -0.16
 
@@ -125,6 +126,7 @@ class LinearProgram:
                             return 0.0
 
                     elif final_number_of_arrows == initial_number_of_arrows + 1 and final_enemy_health == initial_enemy_health:
+
                         if final_stamina == initial_stamina - 1:
                             return -0.64
 
@@ -142,8 +144,16 @@ class LinearProgram:
                     if final_number_of_arrows == self.MAX_ARROWS and initial_number_of_arrows == self.MAX_ARROWS and final_stamina == initial_stamina - 1 and final_enemy_health == initial_enemy_health:
                         return -1
 
-                    elif (final_number_of_arrows == initial_number_of_arrows or final_number_of_arrows == initial_number_of_arrows + 1) and (final_stamina == initial_stamina - 1) and final_enemy_health == initial_enemy_health:
-                        return -0.5
+                    elif (final_stamina == initial_stamina - 1) and final_enemy_health == initial_enemy_health:
+
+                        if final_number_of_arrows == initial_number_of_arrows + 1:
+                            return -0.8
+
+                        elif final_number_of_arrows == initial_number_of_arrows:
+                            return -0.2
+
+                        else:
+                            return 0.0
 
                     else:
                         return 0.0
@@ -228,7 +238,8 @@ np.set_printoptions(threshold=sys.maxsize)
 print(linearProgram.A)
 np.set_printoptions(**opt)
 print()
-# print(np.sum(np.sum(linearProgram.A, 0)))
+print(np.sum(np.sum(linearProgram.A, 0)))
 linearProgram.solve()
 linearProgram.get_policy()
 print(linearProgram.policy)
+print(linearProgram.x)